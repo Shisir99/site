eab3c1bf584faa07314f7b41923ac3948a8cd76d
Shisir99 <shishirchauhan99@gmail.com>
2021-09-20 12:07:40 +0530

    sjdfjf

--- src/components/Contact/EmailLink.js
+++ src/components/Contact/EmailLink.js
@@ -76,7 +76,7 @@ const EmailLink = () => {
       onMouseEnter={() => setIsActive(false)}
       onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
     >
-      <a href={validateText(message) ? `mailto:${message}@gmail.com` : ''}>
+      <a href={validateText(message) ? `mailto:shishirchauhan99@gmail.com` : ''}>
         <span>{message}</span>
         <span>@gmail.com</span>
       </a>
