8b631c4daa521c2a51d24bf309e17c9d7c38696b
Shisir99 <shishirchauhan99@gmail.com>
2021-09-20 09:23:05 +0530

    sjdfjf

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,7 +3,7 @@ const degrees = [
     school: 'IIT Mandi',
     degree: 'B.tech in Electrical Engineering',
     link: 'https://www.iitmandi.ac.in',
-    year: 2017,
+    year: 2021,
   },
   {
     school: 'Bal Vidyapeeth Public School',
